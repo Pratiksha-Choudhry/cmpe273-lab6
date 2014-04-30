67c3569ad9ffaecaa2335a11063ebfef0d095b44
Pratiksha Choudhury <pratiksha70@gmail.com>
2014-04-29 21:29:38 -0700

    Added changes for Lab-6

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,49 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.hash.Hashing;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
-
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+        
+        List<CacheServiceInterface> serverList = new ArrayList<CacheServiceInterface>();
+        serverList.add(new DistributedCacheService("http://localhost:3000"));
+        serverList.add(new DistributedCacheService("http://localhost:3001"));
+        serverList.add(new DistributedCacheService("http://localhost:3002"));
+        
+        CharSequence val;
+        int server;
+        
+        		System.out.println("PUT Key-Value pairs in CacheServer 0, 1, 2... ");
+        
+        		for(int key=1;key<=10;key++)
+        		{
+        			val = randomChar();
+        			server = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), serverList.size());
+        			serverList.get(server).put(key, val.toString());
+        			System.out.println("put (key: "+key +"  value: "+ val+")" +" in CacheServer "+server);
+        		}
+        		
+        		System.out.println("Retrieving all the Key-Value pairs form CacheServer... ");
+        		
+        		for(int key=1;key<=10;key++)
+        		{
+        			server = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), serverList.size());
+        			System.out.println("get (key: "+key +"  value: "+ serverList.get(server).get(key) +")" + "from CacheServer " + server);
+        		}
 
         System.out.println("Existing Cache Client...");
     }
+    
+    public static CharSequence randomChar(){
+    	 		String str ="abcdefghijklmnopqrstuvwxyz";
+    	 		int i = (int)(Math.random()*26);
+    	 		CharSequence s = str.substring(i, i+1);
+    	 		return s;
+    	 	}
 
 }
